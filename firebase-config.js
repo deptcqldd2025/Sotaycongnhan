5eb61784f3dcb6cb5bc10a4f73703873fa1071fc
deptcqldd2025 <deptc.qldd2025@gmail.com>
2026-01-11 07:12:18 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+/* Version: 1.0 */
+const firebaseConfig = {
+  apiKey: "AIzaSyCzWjfcD5Dgq7V-i2izb9tKLR6QM6qWPQo",
+  authDomain: "tavietnam-78ae8.firebaseapp.com",
+  projectId: "tavietnam-78ae8",
+  storageBucket: "tavietnam-78ae8.firebasestorage.app",
+  messagingSenderId: "670121705534",
+  appId: "1:670121705534:web:1027ad98550573ad37116f"
+};
+
+// Khởi tạo Firebase (sẽ được gọi ở các trang)
+// firebase.initializeApp(firebaseConfig);
